3956057a9d712d62bf3f454469228e6dd826f431
siddikaKhushi <sumaiyasiddika449@gmail.com>
2024-10-01 22:54:59 +0600

    ses commit

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,3 @@
 document.getElementById("goToBlogBtn").addEventListener("click", function() {
     window.location.href = "blog.html"; 
 });
-document.getElementById("goToHomeBtn").addEventListener("click", function() {
-    window.location.href = "index.html"; 
-});
